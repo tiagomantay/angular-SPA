65f9321ec197797aaf4faeb83e1b518ddc2e516b
devtiagomantay <devtiagomantay@gmail.com>
2020-01-07 23:30:01 +0000

    Reading tags from component. One way databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-SPA';
+  img_url='http://media1.santabanta.com/full1/Outdoors/Sunset/sunset-199a.jpg'
+  desc='image';
+  
 }
